713a30a8d9e76189ccc88a893c5ed4ae8b339f71
joeyw <wangjq199798@sina.com>
2022-12-15 15:31:06 -0500

    bug fix

--- client/test.py
+++ client/test.py
@@ -11,10 +11,8 @@ import inventory_client
 mock_client = MagicMock()
 mock_client.get_book.side_effect = [
     book_pb2.Book(ISBN = '1000', title = 'I Love API', author = 'Wang', genre = 'Tech',
-                  publishingYear = 2020),
-    book_pb2.Book(ISBN = '1001', title = 'I Love API 2', author = 'Wang', genre = 'Tech',
                   publishingYear = 2021),
-    book_pb2.Book(ISBN = '1002', title = 'I Love API 3', author = 'Wang', genre = 'Tech',
+    book_pb2.Book(ISBN = '1002', title = 'I Love API 2', author = 'Wang', genre = 'Tech',
                   publishingYear = 2022)
 ]
 
@@ -22,7 +20,7 @@ mock_client.get_book.side_effect = [
 class TestInventorySystem(unittest.TestCase):
 
     def test_get_book_titles(self):
-        titles = get_book_titles(mock_client, ['1001', '1002'])
+        titles = get_book_titles(mock_client, ['1000', '1002'])
         self.assertEqual(titles, ['I Love API', 'I Love API 2'])
 
     def test_get_book_titles_with_server(self):
